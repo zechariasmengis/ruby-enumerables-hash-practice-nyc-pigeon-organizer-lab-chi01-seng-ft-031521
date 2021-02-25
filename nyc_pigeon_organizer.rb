1c149587edd4763f57bf063ea095f5628785b883
Zecharias Mengis <zmengis@gmail.com>
2021-02-25 19:24:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,8 +10,8 @@ def nyc_pigeon_organizer(data)
           array_of_names[name][key] = []
         end
         array_of_names[name][key].push(key2)
-        binding.pry
       end
     end
   end
+  binding.pry
 end
