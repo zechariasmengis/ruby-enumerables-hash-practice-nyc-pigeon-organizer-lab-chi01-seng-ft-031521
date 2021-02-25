32d752b3a69d835150be103bf5b68dddf79c8673
Zecharias Mengis <zmengis@gmail.com>
2021-02-25 18:58:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_data_hash = {}
   data.each_with_object({}) do |(key, value), array_of_names|
-    value.collect do |key2, value2|
+    value.each do |key2, value2|
       key2.keys
       binding.pry
     end
