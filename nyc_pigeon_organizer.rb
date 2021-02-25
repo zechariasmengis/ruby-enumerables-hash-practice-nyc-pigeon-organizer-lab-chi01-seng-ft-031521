ccc8919c1055e765cd983337485b369e4e436bb9
Zecharias Mengis <zmengis@gmail.com>
2021-02-25 19:31:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data_hash = data.each_with_object({}) do |(key, value), array_of_names|
+    value.each do |key2, value2|
+      value2.each do |name|
+        if !array_of_names[name]
+          array_of_names[name] = {}
+        end
+        if !array_of_names[name][key]
+          array_of_names[name][key] = []
+        end
+        array_of_names[name][key].push(key2.to_s)
+      end
+    end
+  end
+  new_data_hash
 end
